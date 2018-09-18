84c80e3b5e5a5dfc2787783033b97b6d356f4a9e
路琰 <18637880183@163.com>
2018-09-18 22:06:16 +0800

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div id="app" class="app">
+    <h1>{{msg}}</h1>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import './assets/styles/todo.css'
+export default {
+  name: 'App',
+  data () {
+    return {
+      msg: '123s'
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
